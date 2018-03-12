e8d1a1c2e43a26517982036bc2a23dcaa17663e5
pierventre <pierventre@hotmail.com>
2018-03-11 21:16:27 -0700

    Add configure procedure and subnetting improvements

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import sys
 import re
 
 # Store and endip object
-class EndIP:
+class EndIP(object):
 
   def __init__(self, name, remoteIP, localIntf):
     self.name = name
@@ -21,7 +21,7 @@ class EndIP:
           return "{'name':'%s', 'endip':'%s' 'localintf':'%s'}" %(self.name, self.remoteIP, self.localIntf)
 
 # Store an OSPF network
-class OSPFNetwork:
+class OSPFNetwork(object):
  
   def __init__(self, name, net, cost=1, hello_int=5, area="0.0.0.0"):
 
@@ -39,6 +39,56 @@ class OSPFNetwork:
   
   def __str__(self):
     return "{'name':'%s', 'net':'%s', 'area':'%s'}" %(self.name, self.net, self.area)
+
+# VXLAN tap interface for SRv6Router
+class VNF(object):
+
+  vnf_type = "lxd"
+  layer = "L3"
+  intf = "eth0"
+  bit = 32
+  
+  def __init__(self, name, ip, via, br, net):
+    self.name = name
+    self.id = name.lower()
+    self.ip = ip
+    self.via = via
+    self.br = br
+    self.net = net
+  
+  def serialize(self):
+    serialize = "declare -a %s=(%s %s %s_DEV)\n" \
+    % (self.name, self.vnf_type, self.id, self.name)
+    serialize = serialize + "declare -a %s_DEV=(%s_DEV1)\n" \
+    % (self.name, self.name)
+    serialize = serialize + "declare -a %s_DEV1=(%s %s %s %s %s %s)\n" \
+    % (self.name, self.layer, self.via, self.bit, self.ip, self.intf, self.br)
+    return serialize
+
+  def __str__(self):
+    return "{'name':'%s', type':'%s', 'id': '%s', layer':'%s', 'via':'%s', 'bit': '%s', 'ip':'%s', 'intf:'%s', 'br':'%s'}"  \
+    %(self.name, self.vnf_type, self.id, self.layer, self.via, self.bit, self.ip, self.intf, self.br)
+
+
+# VXLAN tap interface for SRv6Router
+class TERM(object):
+
+  intf = "eth0"
+  bit = 32
+  
+  def __init__(self, name, ip, via, net):
+    self.name = name
+    self.ip = ip
+    self.via = via
+    self.net = net
+  
+  def serialize(self):
+    return "declare -a %s=(%s/%s %s %s/%s)\n" \
+    % (self.name, self.ip, self.bit, self.intf, self.via, self.bit)
+
+  def __str__(self):
+    return "{'name':'%s', 'ip':'%s/%s', 'intf:'%s', 'via':'%s/%s'}"  \
+    %(self.name, self.ip, self.bit, self.intf, self.via, self.bit)
 
 # Allocates loopbacks
 class LoopbackAllocator(object):
@@ -82,30 +132,41 @@ class NetAllocator(object):
     n_net = next(self.ipv6net)
     return n_net
 
-# Allocates subnets for the vnfs
+# Allocates subnets for the ters
 class VNFAllocator(object):
 
   bit = 16
-  net = unicode("fd00::/%s" % bit) 
   
-  def __init__(self):
+  def __init__(self, base):
+    hex_string = format(base, '#04x')
+    hex_string = hex_string[2:]
+    self.net = unicode("fd%s::/%s" % (hex_string, self.bit))
+
     print "*** Calculating Available VNFs Networks"
     self.ipv6net = (IPv6Network(self.net)).subnets(new_prefix=32)
-  
+    for i in range(0, 241):
+      next(self.ipv6net)
+
   def next_netAddress(self):
     n_net = next(self.ipv6net)
     return n_net
 
 # Allocates subnets for the ters
-class TERAllocator(object):
+class TERMAllocator(object):
 
   bit = 16
-  net = unicode("fd01::/%s" % bit) 
   
-  def __init__(self):
-    print "*** Calculating Available TERs Networks"
+  def __init__(self, base):
+    hex_string = format(base, '#04x')
+    hex_string = hex_string[2:]
+    self.net = unicode("fd%s::/%s" % (hex_string, self.bit))
+
+    print "*** Calculating Available TERMs Networks"
     self.ipv6net = (IPv6Network(self.net)).subnets(new_prefix=32)
-  
+    next(self.ipv6net)
+
   def next_netAddress(self):
     n_net = next(self.ipv6net)
     return n_net
+
+
